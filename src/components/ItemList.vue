48ac5d9dcc3e4afe8339ed01373d091f1152c373
JINBO <kimpa0107@163.com>
2021-04-12 19:24:22 +0800

    init

---
+++ src/components/ItemList.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-table :data="items">
+    <el-table-column label="name" prop="name"></el-table-column>
+    <el-table-column label="child">
+      <template v-slot="{ row }">{{ row.child.id }}</template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+import { reactive } from 'vue';
+export default {
+  setup() {
+    const items = reactive([]);
+
+    for (let i = 0; i < 10; i++) {
+      const num = i + 1;
+      items.push({
+        id: num,
+        name: `name - ${num}`,
+        child: {
+          id: num * 10,
+        },
+      });
+    }
+
+    return { items };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
